826a80c8c71fdfe8c8acad5b23d7d84ec3c6a68b
Kenshi1453 <kenshi1453@gmail.com>
2025-02-18 20:24:48 +0100

    make it so you can now have different triggers for different queues. Also add some documentation

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { AsyncQueue } from "./queue.js";
 import { PiShockAPI } from "./pishock.js";
 import { sleep, waitUntil } from "./utils.js";
 import { BaseTrigger, ContinuousTrigger, ImmediateDontHaveTrigger, ImmediateMustHaveTrigger } from "./types-of-triggers.js";
-import { getEarliestOfAsyncIterators } from "./utils.js";
 
 export type QueueType = {
   intensity: number;
@@ -13,30 +12,68 @@ export type QueueType = {
   author: string;
 }
 
-// use | null for the queue even tho it will never be null teehee i love typescript :ppp
-const WHO_TO_STALK: { id: string, triggers: BaseTrigger[], queue: AsyncQueue<QueueType> | null }[] = [
-  {
-    id: "1",
-    triggers: [
-      // add your triggers
-    ],
-    queue: null
-  },
-  {
-    id: "2",
-    triggers: [
-      // add your triggers
-    ],
-    queue: null
-  }
-];
+type QueueListType = { queue: AsyncQueue<QueueType>, callback: (v: QueueType) => number }
+type StalkType = { id: string, triggers: BaseTrigger[], queue: QueueListType }
 
-const SHOCK_QUEUES: AsyncQueue<QueueType>[] = []
-for (const stalk of WHO_TO_STALK) {
-  const queue = new AsyncQueue<QueueType>();
-  SHOCK_QUEUES.push(queue);
-  stalk.queue = queue;
+function generateQueueAndStalking(): [StalkType[], QueueListType[]] {
+
+
+
+  const WHO_TO_STALK: { id: string, triggers: BaseTrigger[], queue: QueueListType }[] = [
+    {
+      // first queue
+      id: "1",
+      triggers: [
+        // This is a continuous trigger. The first list is the list of the trigger words. When one of them has been said, they will be 
+        // constantly be triggered after a set interval (twice the duration parameter) until the person says the counter-word.
+        // in this example the trigger word is "weeb" and the counter "word" is 'I deeply apologize for having upset you, oh japanese master'
+        new ContinuousTrigger(1, 1, ["weeb"], ["I deeply apologize for having upset you, oh japanese master"]),
+        // This is a trigger that means that you *need* to always have the trigger word in each message or else it will trigger.
+        // so each message that doesn't contain "owo" are 1 trigger.
+        new ImmediateMustHaveTrigger(2, 2, ["owo"], true),
+        // This is a trigger that means that you should NEVER have the trigger word in your message.
+        // It counts **occurences** of the word. So the message "ewe ewe ewe" is effectively 3 triggers
+        new ImmediateDontHaveTrigger(3, 3, ["ewe"]),
+      ],
+      queue: {
+        queue: new AsyncQueue<QueueType>(),
+        callback: (v) => { console.log(`GET PUNISHED IDIOT ('${v.message}')`); return 2 * v.duration * 1000 }
+      },
+    },
+    {
+      // second queue
+      // this one uses the same id as the previous item because I want to show how to have different queue actions AND timings
+      // for the same person but with different triggers.
+      id: "1",
+      triggers: [
+        // Shows how different items could interact. In the previous we have a "MustHave" type of trigger, but for this specific queue with 
+        // a different action on trigger, it is instead a DontHave, meaning we can weave those triggers together
+        new ImmediateDontHaveTrigger(2, 2, ["owo"], true),
+      ],
+      queue: {
+        queue: new AsyncQueue<QueueType>(),
+        callback: (v) => { console.log(`GET REWARDED IDIOT ('${v.message}')`); return v.duration * 1000 }
+      },
+    },
+    {
+      // someone else
+      id: "2",
+      triggers: [
+        new ImmediateDontHaveTrigger(50, 15, ["lmao"], true)
+      ],
+      queue: {
+        queue: new AsyncQueue<QueueType>(),
+        callback: (v) => { console.log(`Yet another completely different message ('${v.message}')`); return 2 * v.duration * 1000 }
+      },
+    }
+  ];
+
+  return [WHO_TO_STALK, WHO_TO_STALK.map((v) => v.queue)]
 }
+
+const [WHO_TO_STALK, QUEUES] = generateQueueAndStalking();
+
+// use | null for the queue even tho it will never be null teehee i love typescript :ppp
 
 let SHOULD_READ_QUEUE = true;
 
@@ -61,7 +98,7 @@ client.on("messageCreate", (message) => {
     if (author.id !== stalk.id) continue;
 
     for (const trigger of stalk.triggers) {
-      trigger.checkMessageAndDoShit(content.toLowerCase(), stalk.queue!, author.displayName)
+      trigger.checkMessageAndDoShit(content.toLowerCase(), stalk.queue.queue, author.displayName)
     }
   }
 });
@@ -78,15 +115,15 @@ const pishockAPI = new PiShockAPI(
 
 process.stdin.on("data", (d) => {
   const str = d.toString("utf-8").trim();
-  if(str === "pause"){
+  if (str === "pause") {
     SHOULD_READ_QUEUE = false
-  } else if(str === "resume"){
+  } else if (str === "resume") {
     SHOULD_READ_QUEUE = true;
   }
 })
 
 
-function doTheFunny(q: AsyncQueue<QueueType>): () => void {
+function doTheFunny(q: AsyncQueue<QueueType>, callback: (v: QueueType) => number): () => void {
   return () => {
     q.dequeue().then(v => {
       // console.log("Waiting for queue input...");
@@ -96,12 +133,13 @@ function doTheFunny(q: AsyncQueue<QueueType>): () => void {
       }
       return v
     }).then(v => {
-      // Do funnies with the thing you got :3
-      setTimeout(doTheFunny(q), 2 * v.duration * 1000)
+      // BLABLA GOT SHOCKED BY A 10S WITH INTENSITY 50 (DUE TO MESSAGE "blabla" [INSTANT]) SHOCK
+      const sleepTime = callback(v);
+      setTimeout(doTheFunny(q, callback), sleepTime)
     })
   }
 }
 
-for (const q of SHOCK_QUEUES) {
-  setTimeout(doTheFunny(q))
+for (const q of QUEUES) {
+  setTimeout(doTheFunny(q.queue, q.callback))
 }
